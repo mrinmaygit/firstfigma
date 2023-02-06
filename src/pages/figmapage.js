47d272db3ec6f3d7a7d9fd00551154263183261f
Mrinmay <mrinmay@hyperhire.in>
2023-02-06 11:10:26 +0530

    first commit

---
+++ src/pages/figmapage.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export default function figmapage() {
+  return (
+    <div>
+
+
+    <div  className=" up_background d-flex flex-row justify-content-center ">
+        
+        
+        <div>
+            <img src="https://res.cloudinary.com/dn14yrrye/image/upload/v1675149923/Mask_group_3_y1slla.png"/>
+       </div>
+    
+        
+     <div>
+        <h1>Sign up and complete your profile.</h1>
+        <p>
+            Answer a few questions about your experience & skill.
+            upload your resume  - it takes less than 3 minutes to .
+            complete your profile on zigup and get direct access to.
+            hiring managers.
+        </p>
+        <p>
+            Apply for relevant Jobs from our job section and sit back.
+            while we review your profile for the best job.
+        </p>
+    
+     </div>
+
+    </div>
+
+
+    <div  className=" low_background d-flex flex-row justify-content-center">
+       
+        
+       <div>
+         <h1>
+            Receive Job offers. Get hired.
+         </h1>
+         <p>
+            Get interview requests from top startups, usually within 2-3 days.
+            Our HR screening process is doesn't take more than 30 mins, and 
+            the final interview takes only an hours. 
+         </p>
+
+         <p>
+            Your time matter, and our interview exprience is designed to . 
+            ensure you receive timely feedback and offeers. We make job .
+            offers within 3-7 Days.
+         </p>
+
+        </div>
+
+    
+      <div>
+           <img src="https://res.cloudinary.com/dn14yrrye/image/upload/v1675149923/Rectangle_1_2_fllvv7.png"/>
+      </div>
+
+    </div>
+
+    </div>
+  )
+}
